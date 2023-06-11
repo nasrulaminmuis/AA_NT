afe027f6610f6f0f6b4bff3ea28f91fd5b7c49ff
nasrulaminmuis <nasrulaminmuis68@gmail.com>
2023-06-11 14:14:21 +0700

    gabungin punya tri karena eror pas di production

---
+++ src/beranda.js
@@ -0,0 +1,5 @@
+AOS.init({
+    duration: 1000, // Durasi animasi dalam milidetik
+    once: true, // Animasi hanya akan terjadi sekali saat elemen pertama kali masuk viewport
+    mirror: true, // Animasi akan terjadi saat elemen masuk viewport baik saat di-scroll ke bawah maupun ke atas
+  });
